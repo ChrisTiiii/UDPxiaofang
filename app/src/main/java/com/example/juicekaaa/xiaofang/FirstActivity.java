25cf22a6fbe286c09c1b84cfb65ad688a8148506
Juicekaaa <454837243@qq.com>
2018-11-27 09:29:33 +0800

    version1.0

---
+++ app/src/main/java/com/example/juicekaaa/xiaofang/FirstActivity.java
@@ -0,0 +1,76 @@
+package com.example.juicekaaa.xiaofang;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.GridView;
+import android.widget.SimpleAdapter;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+/**
+ * author: ZhongMing
+ * DATE: 2018/11/27 0027
+ * Description:
+ **/
+public class FirstActivity extends AppCompatActivity {
+    @BindView(R.id.gridview)
+    GridView gridview;
+    private List<Map<String, Object>> dataList;
+    private SimpleAdapter adapter;
+
+
+    final int img[] = new int[]{R.drawable.wz, R.drawable.opendoor, R.drawable.qd,
+            R.drawable.yh, R.drawable.msg, R.drawable.hd, R.drawable.qz, R.drawable.person, R.drawable.sq};
+
+    final String text[] = new String[]{"物资管理", "应急开门", "签到巡逻", "隐患处理"
+            , "我的消息", "培训活动", "在线求助", "个人中心", "授权中心"};
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.first_activity);
+        ButterKnife.bind(this);
+        //初始化数据
+        initData();
+        String[] from = {"img", "text"};
+        int[] to = {R.id.img, R.id.text};
+        adapter = new SimpleAdapter(this, dataList, R.layout.first_item, from, to);
+        gridview.setAdapter(adapter);
+        gridview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> arg0, View arg1, int position,
+                                    long arg3) {
+                switch (position) {
+                    case 1:
+                        startActivity(new Intent(FirstActivity.this, MainActivity.class));
+                        break;
+                }
+
+            }
+        });
+
+
+    }
+
+    private void initData() {
+        dataList = new ArrayList<>();
+        for (int i = 0; i < img.length; i++) {
+            Map<String, Object> map = new HashMap<>();
+            map.put("img", img[i]);
+            map.put("text", text[i]);
+            dataList.add(map);
+        }
+
+    }
+}
